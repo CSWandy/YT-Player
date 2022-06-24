5c6c1e190a7a407740f727d7765059678ad91782
Shisheloma <shisheloma@gmail.com>
2022-06-24 16:04:50 +0300

    Add common hub for Routes export

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 
-import Layout from './components/Routes/Layout/Layout';
-import Subscriptions from './components/Routes/Subscriptions/Subscriptions';
-import Liked from './components/Routes/Liked/Liked';
-import History  from './components/Routes/History/History';
-import Search  from './components/Routes/Search/Search';
-import Watch  from './components/Routes/Watch/Watch';
-import Playlists  from './components/Routes/Playlists/Playlists';
-import Playlist  from './components/Routes/Playlist/Playlist';
-import Channel from './components/Routes/Channel/Channel';
-import Privacy from './components/Routes/Privacy/Privacy';
+import AppRoutes from './components/Routes/';
 
 function App() {
 
-  return (
-    <Routes>
-      <Route index element={<Navigate to="/playlist" replace/>} />
-      <Route path="*" element={<Navigate to="/playlist" replace/>} />
-      <Route path="/" element={<Layout/>}>
-        <Route path="subscriptions" element={ <Subscriptions/>} />
-        <Route path="liked" element={ <Liked/>} />
-        <Route path="history" element={ <History/>} />
-        <Route path='watch/:vidId' element={<Watch/>} /> 
-        <Route path='search/:query' element={<Search/>} /> 
-        <Route path='channel/:channelId' element={<Channel/>} /> 
-        <Route path='playlist/' element={<Outlet/>} >
-          <Route index element={<Playlists/>} />
-          <Route path=':playlistId' element={<Playlist/>} /> 
-        </Route> 
-        <Route path='privacy' element={<Privacy/>} /> 
-      </Route>
-    </Routes>
-  )
+const {
+    Channel,
+    Layout,  
+    Subscriptions, 
+    Liked, 
+    History, 
+    Search, 
+    Watch, 
+    Playlists, 
+    Playlist, 
+    Privacy
+} = AppRoutes;
+
+    return (
+        <Routes>
+        <Route index element={<Navigate to="/playlist" replace/>} />
+        <Route path="*" element={<Navigate to="/playlist" replace/>} />
+        <Route path="/" element={<Layout/>}>
+            <Route path="subscriptions" element={ <Subscriptions/>} />
+            <Route path="liked" element={ <Liked/>} />
+            <Route path="history" element={ <History/>} />
+            <Route path='watch/:vidId' element={<Watch/>} /> 
+            <Route path='search/:query' element={<Search/>} /> 
+            <Route path='channel/:channelId' element={<Channel/>} /> 
+            <Route path='playlist/' element={<Outlet/>} >
+                <Route index element={<Playlists/>} />
+                <Route path=':playlistId' element={<Playlist/>} /> 
+            </Route> 
+            <Route path='privacy' element={<Privacy/>} /> 
+        </Route>
+        </Routes>
+    )
 }
 
 export default App
---
+++ src/components/Routes/index.js
@@ -0,0 +1,27 @@
+import Channel from "./Channel/Channel";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
+import Layout from './Layout/Layout';
+import Subscriptions from './Subscriptions/Subscriptions';
+import Liked from './Liked/Liked';
+import History  from './History/History';
+import Search  from './Search/Search';
+import Watch  from './Watch/Watch';
+import Playlists  from './Playlists/Playlists';
+import Playlist  from './Playlist/Playlist';
+import Privacy from './Privacy/Privacy';
+
+const AppRoutes = {
+    Channel,
+    ErrorBoundary,
+    Layout, 
+    Subscriptions,
+    Liked,
+    History,
+    Search,
+    Watch,
+    Playlists,
+    Playlist,
+    Privacy
+};
+
+export default AppRoutes
